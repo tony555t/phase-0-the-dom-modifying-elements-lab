1c57b5139852135e811d430bbd1634e989527fcb
tony555t <tonytullah764@gmail.com>
2022-11-23 08:17:09 +0300

    modification

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+
+main.remove();
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "tyson is the champion";
